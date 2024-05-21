9a271e8d2f6edb01f22d165f8c92e5f71cfdeb4d
Tabish <tabish786qidwai@gmail.com>
2024-05-21 06:24:20 +0530

    commit

--- amazon_price_scraper.py
+++ amazon_price_scraper.py
@@ -8,11 +8,19 @@ BASE_URL = 'https://www.amazon.in'
 def get_links_from_page(page=1):
     SEARCH_PAGE_URL = f'https://www.amazon.in/s?k=laptops&page={page}'
     scraper = Scraper(SEARCH_PAGE_URL)
-    target = Tag(cls='s-main-slot s-result-list s-search-results sg-row')
-    items = Tag(cls='s-result-item')
+    target = Tag(cls='s-desktop-width-max s-desktop-content s-opposite-dir s-wide-grid-style sg-row')
+    items = Tag(cls='puisg-row')
     link = Tag('a', cls='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal', output='href')
 
-    all_links = scraper.get_all(target, items, link=link)
+    '''
+    the Scraper method get_repeating data isn't returning anything, its displaying none
+    the tags are not correct might be
+    or the function has some issues in the library dputils. 
+    runs after many tries for the previous tags, specific tag i searched
+    '''
+
+    all_links = scraper.get_repeating_page_data(target = target, items = items, link=link)
+    print(len(all_links))
     return all_links
 
 def get_laptop_details(link):
@@ -29,7 +37,7 @@ def get_laptop_details(link):
     13.Price ✔️
     '''
     scraper = Scraper(link, clean=True)
-    manufacturer = Tag('tr', cls='po-brand')
+    manufacturer = Tag('span', cls='a-size-base po-break-word')
     model_name = Tag('tr', cls='po-model_name')
     screen_size = Tag('tr', cls='po-display.size')
     color = Tag('tr', cls='po-color')
@@ -40,7 +48,7 @@ def get_laptop_details(link):
     gpu = Tag('tr', cls='po-graphics_coprocessor')
     price =Tag('span', cls='a-price-whole')
 
-    result =  scraper.get(brand=manufacturer, 
+    result =  scraper.get_page_data(brand=manufacturer, 
                        model=model_name, 
                        screen_size=screen_size, 
                        color=color, 
@@ -60,7 +68,7 @@ def main(limit=10):
         links = [f"{BASE_URL}{link['link']}" for link in get_links_from_page(page) if link['link'] !=None]
         laptops = []
         if len(links) > 0:
-            print(f'Found {len(links)} links')
+            print(f'Found {len(links)} links') 
             for link in links:
                 print(f'Extracting details from {link}')
                 details = get_laptop_details(link)
@@ -79,8 +87,4 @@ def main(limit=10):
 
 if __name__ == '__main__':
     main()
-
-
-
-
 
